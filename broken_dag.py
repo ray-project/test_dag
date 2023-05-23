81c1912273e512a3756e359e08900f0e5b2e1811
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-05-23 13:13:45 -0700

    Import matplotlib
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- broken_dag.py
+++ broken_dag.py
@@ -3,8 +3,8 @@ from ray import serve
 class CustomException(Exception):
 
     def __init__(self, *args):
-        import numpy as np
-        self.zeros = np.zeros(5)
+        import matplotlib
+        self.version = matplotlib.__version__
 
 raise CustomException("This is a custom exception!")
 
