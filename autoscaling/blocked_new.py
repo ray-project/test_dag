23eac0533522579dff9a8a1ecb88931874d987ea
Cindy Zhang <cindyzyx9@gmail.com>
2023-09-05 17:15:14 -0700

    fix
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- autoscaling/blocked_new.py
+++ autoscaling/blocked_new.py
@@ -1,12 +1,10 @@
-import ray
 from ray import serve
 
 @serve.deployment
 class Blocked:
-    def __call__(self):
+    async def __call__(self):
         signal = serve.get_deployment_handle("SignalDeployment", app_name="signal")
-        ref = signal.wait.remote()
-        ref.result()
+        await signal.wait.remote()
         return "hello"
 
 app = Blocked.bind()
