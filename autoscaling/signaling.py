59c994656f55ff8e0e642efe63f380debc831931
Cindy Zhang <cindyzyx9@gmail.com>
2023-12-28 10:10:06 -0800

    remove dagdriver
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- autoscaling/signaling.py
+++ autoscaling/signaling.py
@@ -1,14 +1,6 @@
 import asyncio
-import starlette.requests
-from typing import Any, Dict
 
 from ray import serve
-from ray.serve.drivers import DAGDriver
-
-async def json_request(request: starlette.requests.Request) -> Dict[str, Any]:
-    if len(await request.body()) == 0:
-        return {}
-    return await request.json()
 
 @serve.deployment(ray_actor_options={"num_cpus": 0})
 class SignalDeployment:
@@ -23,4 +15,4 @@ class SignalDeployment:
     async def wait(self):
         await self.ready_event.wait()
 
-app = DAGDriver.bind(SignalDeployment.bind(), http_adapter=json_request)
+app = SignalDeployment.bind()
